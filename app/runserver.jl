47fbfa48dbd4512d1ff51e98a1818ddebc0fa5cc
Michael Klamkin <michael@klamkin.com>
2025-05-31 18:32:02 -0400

    restore logging

--- app/runserver.jl
+++ app/runserver.jl
@@ -8,17 +8,16 @@ LPVizHTTPExt = Base.get_extension(LPViz, :LPVizHTTPExt)
 
 
 if abspath(PROGRAM_FILE) == @__FILE__
-    verbose = true
+    verbose = false
 
     # Run each handler once
-    LPVizHTTPExt.precompile_handlers(verbose)
     LPVizHTTPExt.fontpath() = artifact"JuliaMono" * "/webfonts/JuliaMono-Light.woff2"
     # Run the server forever
-    # LoggingExtras.withlevel(verbose ? LoggingExtras.Debug : LoggingExtras.Info) do
-    LPVizHTTPExt.runserver(
-        port=parse(Int, get(ARGS, 1, "8080")),
-        expose=length(ARGS) > 1 && ARGS[2] == "expose",
-        verbose=verbose
-    )
-    # end
+    LoggingExtras.withlevel(verbose ? LoggingExtras.Debug : LoggingExtras.Info) do
+        LPVizHTTPExt.runserver(
+            port=parse(Int, get(ARGS, 1, "8080")),
+            expose=length(ARGS) > 1 && ARGS[2] == "expose",
+            verbose=verbose
+        )
+    end
 end
