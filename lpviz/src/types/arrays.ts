e490c494fb6f2bcefa730f318ea74aed8009717c
Michael Klamkin <michael@klamkin.com>
2025-09-01 00:10:45 -0400

    refactor central path internals to ml-matrix

--- lpviz/src/types/arrays.ts
+++ lpviz/src/types/arrays.ts
@@ -1,3 +1,4 @@
+import {Matrix} from 'ml-matrix';
 // for 3D visuals
 export interface PointXY { x: number; y: number; }
 export interface PointXYZ { x: number; y: number; z: number; }
@@ -8,6 +9,9 @@ export type Vec3 = number[]; // [x, y, z] or [A, B, C]
 export type VecM = number[]; // M refers to number of constraints, e.g. for the barrier weights vector
 export type VecN = number[]; // N refers to number of variables, e.g. for the objective vector
 export type ArrayMatrix = number[][];
+
+export type VectorM = Matrix;
+export type VectorN = Matrix;
 
 // for pdhg and simplex
 export type Vec2N = number[];
