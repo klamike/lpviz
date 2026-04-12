5e37fe8493c25cc26c0a3c99c605f9ec3606d54a
thaske <10328778+thaske@users.noreply.github.com>
2026-04-12 19:03:55 -0400

    Move overlay visibility into React

--- lpviz/src/app/AppShell.tsx
+++ lpviz/src/app/AppShell.tsx
@@ -1,4 +1,7 @@
+import { useEffect, useState } from "react";
+
 import { LegacyRuntimeElementsProvider } from "./legacyRuntimeElements";
+import { MIN_SCREEN_WIDTH } from "./uiConstants";
 import { CanvasStage } from "../components/layout/CanvasStage";
 import { Sidebar } from "../components/layout/Sidebar";
 
@@ -9,6 +12,28 @@ export function AppShell() {
         <Sidebar />
       </header>
       <CanvasStage />
+      <SmallScreenOverlay />
     </LegacyRuntimeElementsProvider>
+  );
+}
+
+function SmallScreenOverlay() {
+  const [windowWidth, setWindowWidth] = useState(() => window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  const tooSmall = windowWidth < MIN_SCREEN_WIDTH;
+
+  return (
+    <div id="smallScreenOverlay" className={`small-screen-overlay${tooSmall ? " is-flex" : " is-hidden"}`}>
+      {`The window is not wide enough (${windowWidth}px < ${MIN_SCREEN_WIDTH}px) for lpviz.`}
+    </div>
   );
 }
